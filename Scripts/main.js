4ac75897a4ce6cccc3a3ba6919e5637d556b141b
JeskaLopes <jessica97cantora@hotmail.com>
2019-06-27 12:42:05 -0300

    adiciona conteudo

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,17 +6,15 @@ let cargo = document.getElementById('text');
 let empresa = document.getElementById('text2')
 let campo = document.getElementById('exampleFormControlTextarea1')
 
-/*
+
 form.addEventListener('submit', function(e) {
     // alerta o valor do nome
     if(nome.value == "" | sobrenome.value =="" | cargo.value == "" | empresa.value == "" | campo.value == ""){
         alert("Preencha todos os campos")
     }
-
-    // impede o envio do form
     e.preventDefault();
 });
- */
+
 
 $('#formulario').submit(function(e) {
     e.preventDefault()
